ee92f9fcfb2cd64a7fcb77f49941e832cadbbd1a
Gochad <malgorzata.drag@spoton.com>
2024-05-12 19:58:07 +0200

    Updates

--- src/pages/styles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const AppContainer = styled.footer`
-    min-height: 100vh;
-    position: relative;
-    padding-bottom: 60px;
-    overflow-x: hidden;
-`;
