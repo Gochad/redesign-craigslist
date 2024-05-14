7ae562b34c50cde1003de8b985c9b078bc87e783
Gochad <malgorzata.drag@spoton.com>
2024-05-14 16:45:52 +0200

    remove hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import { SearchProvider } from './context/SearchContext';
 import HomePage from './pages/HomePage';
